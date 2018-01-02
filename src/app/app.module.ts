c03e63218422a07d22800d47b82586dd52066bef
Vladimir Sinyavsky <sva.sorcery@gmail.com>
2018-01-02 13:10:38 +0300

    [+] SpinnerComponent : created, closes #5

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
-
 
 import { AppComponent } from './app.component';
 
+import { SpinnerComponent } from './components/spinner.component';
+
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    imports: [
+        BrowserModule,
+        CommonModule
+    ],
+    declarations: [
+        AppComponent,
+        SpinnerComponent
+    ],
+    providers: [
+
+    ],
+    bootstrap: [
+        AppComponent
+    ]
 })
 export class AppModule { }
