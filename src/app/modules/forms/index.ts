f73324c8b8e92b03491c33ae513d4410257e6355
Vladimir Sinyavsky <sva.sorcery@gmail.com>
2019-05-13 10:40:50 +0300

    [+] FormControl ElementBase : created, closes #95

--- src/app/modules/forms/index.ts
+++ src/app/modules/forms/index.ts
@@ -2,3 +2,7 @@ export * from './forms.module';
 export * from './forms.abstractions';
 export * from './simple-form.component';
 export { LockingService } from '../locking/locking.service';
+
+export * from './element/element-base';
+export * from './element/value-accessor';
+export * from './element/validate';
